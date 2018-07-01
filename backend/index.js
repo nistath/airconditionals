f71c3e1e8e57af805eec9e01ea8eda80ec7a6c8d
Nick Stathas <nickstathas@outlook.com>
2018-07-01 11:48:56 +0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,12 @@
-'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const api = require('./Routes/api');
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+const app = express();
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+app.use('/api',api);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+app.listen(3003,()=>{console.log('Server listening on port 3003')});
